c57a51c39d3d4a1407c56ad2a6a083701f0c7e66
Hanky Liu <103504158+zihanltesla@users.noreply.github.com>
2024-03-31 15:20:15 +0200

    Define websocket message type #66 #67

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/constant/MessageType.java
@@ -0,0 +1,8 @@
+package ch.uzh.ifi.hase.soprafs24.constant;
+
+public enum MessageType {
+	MESSAGE,
+	JOIN,
+	LEAVE,
+	READY
+}
--- src/main/java/ch/uzh/ifi/hase/soprafs24/model/Message.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/model/Message.java
@@ -6,58 +6,59 @@ import lombok.NoArgsConstructor;
 
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 import ch.uzh.ifi.hase.soprafs24.constant.MessageStatus;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+
 public class Message {
-	private String senderName;
-	private String receiverName;
-	private String message;
-	private String date;
-	private MessageStatus MessageStatus;
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private LocalDateTime timestamp; // Changed from String date to LocalDateTime timestamp
+    private MessageStatus messageStatus; // Changed field name to camelCase
 
+    // Getters and setters
+    public String getSenderName() {
+        return senderName;
+    }
 
-	public String getSenderName() {
-		return senderName;
-	}
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
 
-	public void setSenderName(String senderName) {
-		this.senderName = senderName;
-	}
+    public String getReceiverName() {
+        return receiverName;
+    }
 
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
 
-	public String getReceiverName() {
-		return receiverName;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public void setReceiverName(String receiverName) {
-		this.receiverName = receiverName;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 
-	public String getDate() {
-		return date;
-	}
+    public MessageStatus getMessageStatus() {
+        return messageStatus;
+    }
 
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public MessageStatus getMessageStatus() {
-		return MessageStatus;
-	}
-
-	public void setStatus(MessageStatus MessageStatus) {
-		this.MessageStatus = MessageStatus;
-	}
-
+    public void setMessageStatus(MessageStatus messageStatus) {
+        this.messageStatus = messageStatus;
+    }
 }
