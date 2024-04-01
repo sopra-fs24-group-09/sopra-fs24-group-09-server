6ee5cebc9afbde2ed2da9e3ed9bff315a19797cb
Hanky Liu <103504158+zihanltesla@users.noreply.github.com>
2024-04-01 02:30:05 +0200

    Apply game service for game processing #91

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/SocketController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/SocketController.java
@@ -1,32 +1,39 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
+import ch.uzh.ifi.hase.soprafs24.service.GameService;
 import ch.uzh.ifi.hase.soprafs24.service.RoomService;
 import ch.uzh.ifi.hase.soprafs24.service.UserService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.server.ResponseStatusException;
+
 import ch.uzh.ifi.hase.soprafs24.service.SocketService;
 import ch.uzh.ifi.hase.soprafs24.model.Message;
 import ch.uzh.ifi.hase.soprafs24.constant.MessageOrderType;
+import ch.uzh.ifi.hase.soprafs24.entity.Player;
 @Controller
 public class SocketController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final SocketService socketService;
+    private final GameService gameService;
 
-    public SocketController(SocketService socketService, RoomService roomService, UserService userService, SimpMessagingTemplate simpMessagingTemplate) {
+    public SocketController(SocketService socketService, RoomService roomService, UserService userService, GameService gameService, SimpMessagingTemplate simpMessagingTemplate) {
         this.simpMessagingTemplate = simpMessagingTemplate;
         this.socketService = socketService;
+        this.gameService=gameService;
     }
 
     //set ready
     @MessageMapping("/message/{userId}/{roomId}/ready")
     @SendTo("/room/{roomId}/public")
     public Message receiveReadyMessage(@Payload Message message,@DestinationVariable("userId") Long userId,@DestinationVariable("roomId") Long roomId) {
-        // setready(userid)
+        gameService.Ready(userId);
         socketService.broadcastReady(roomId, true);
         Message wordMessage = new Message();
         wordMessage.setSenderName("system");
@@ -40,13 +47,12 @@ public class SocketController {
     @MessageMapping("/message/{userId}/{roomId}/unready")
     @SendTo("/room/{roomId}/public")
     public Message receiveUnreadyMessage(@Payload Message message,@DestinationVariable("userId") Long userId,@DestinationVariable("roomId") Long roomId) {
-        // setUnready(userid)
+        gameService.UnReady(userId);
         socketService.broadcastReady(roomId, false);
         Message wordMessage = new Message();
         wordMessage.setSenderName("system");
-        wordMessage.setMessageType(MessageOrderType.LEAVE);
+        wordMessage.setMessageType(MessageOrderType.UNREADY);
         simpMessagingTemplate.convertAndSend("/room/"+roomId+"/public",  wordMessage);
-        
         return message;
     }
 
--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Player.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Player.java
@@ -1,14 +1,26 @@
 package ch.uzh.ifi.hase.soprafs24.entity;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import ch.uzh.ifi.hase.soprafs24.constant.PlayerStatus;
+
 @Document(collection = "player") 
 public class Player extends User {
 
+    private PlayerStatus playerStatus; 
     private String audioData; 
     private String word; 
 
     public Player() {
         super();
+    }
+
+
+    public PlayerStatus getPlayerStatus() {
+        return playerStatus;
+    }
+
+    public void setPlayerStatus(PlayerStatus playerStatus) {
+        this.playerStatus= playerStatus;
     }
 
     public String getAudioData() {
