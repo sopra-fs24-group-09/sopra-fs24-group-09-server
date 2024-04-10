43b227a837a2bbec352500d6a2baba2e896d3171
Zehao-Zhang <zehao.zhang@uzh.ch>
2024-04-10 17:01:43 +0200

    add entity and workflow for develop branch

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/GameRepository.java
@@ -0,0 +1,20 @@
+package ch.uzh.ifi.hase.soprafs24.repository;
+//import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs24.entity.Game;
+import ch.uzh.ifi.hase.soprafs24.entity.Room;
+import  org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository("gameRepository")
+public interface GameRepository extends MongoRepository<Game, String> {
+    Optional<Game> findByRoomId(String roomId);
+
+    @Override
+    List<Game> findAll();
+}
+
